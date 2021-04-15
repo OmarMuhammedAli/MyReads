e563b5b2d11029c00ada5ae00ee725f24b83f9a5
Omar <omarmuhammed1998.om@gmail.com>
2021-04-15 04:20:51 +0200

    Managed routing in the app

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
 import React from 'react'
+import {Route, Switch, Redirect} from 'react-router-dom'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 import { 
   BooksView,
-  SearchView
+  SearchView,
+  PageNotFound
  } from './views'
+ 
 
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    
   }
 
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <SearchView/>
-        ) : (
-          <BooksView/>
-        )}
+        <Switch>
+          <Route exact path='/' render={() => <BooksView/>}/>
+          <Route exact path='/search' render={() => <SearchView/>}/>
+          <Route exact path='/404' component={PageNotFound}/>
+          <Redirect to='/404'/>
+        </Switch>
       </div>
     )
   }
