82b616a77c5ad91ede62588028040502fd37a182
Omar <omarmuhammed1998.om@gmail.com>
2021-04-15 05:45:19 +0200

    Dynamically creating book shelves. Added PropTypes

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,21 +1,25 @@
 import React from "react";
 import Book from './Book'
+import PropTypes from 'prop-types'
 
-const BookShelf = (props) => {
+const BookShelf = ({title, books, value}) => {
   return (
     <div className="bookshelf">
-      <h2 className="bookshelf-title">Currently Reading</h2>
+      <h2 className="bookshelf-title">{title}</h2>
       <div className="bookshelf-books">
         <ol className="books-grid">
-          <li>
-            <Book />
-          </li>
-          <li>
-            <Book />
-          </li>
+          {books.map(book => (
+            <li key={book.id}><Book shelfValue={value} book={book}/></li>
+          ))}
         </ol>
       </div>
     </div>
   );
 };
+
+BookShelf.propTypes = {
+  title: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  books: PropTypes.array.isRequired,
+}
 export default BookShelf;
