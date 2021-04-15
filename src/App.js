51021b83ca2363ceb7cf1db2f8f6305c577a92d0
Omar <omarmuhammed1998.om@gmail.com>
2021-04-15 05:44:26 +0200

    BooksAPI integration in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Route, Switch, Redirect} from 'react-router-dom'
+import BooksAPI from './api'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 import { 
@@ -8,19 +9,26 @@ import {
   PageNotFound
  } from './views'
  
-
-
 class BooksApp extends React.Component {
-  state = {
-    
+  constructor(props) {
+    super(props)
+    this.state = {
+      books: []
+    }
   }
-
+  async componentDidMount() {
+    const books = await BooksAPI.getAll()
+    this.setState({
+      books
+    })
+    console.log(this.state.books)
+  }
   render() {
     return (
       <div className="app">
         <Switch>
-          <Route exact path='/' render={() => <BooksView/>}/>
-          <Route exact path='/search' render={() => <SearchView/>}/>
+          <Route exact path='/' render={() => <BooksView books={this.state.books}/>}/>
+          <Route exact path='/search' render={() => <SearchView books={this.state.books}/>}/>
           <Route exact path='/404' component={PageNotFound}/>
           <Redirect to='/404'/>
         </Switch>
