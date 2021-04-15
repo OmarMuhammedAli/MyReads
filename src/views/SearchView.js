b3722353d0a87c336d8e89953277c03dc54d13a7
Omar <omarmuhammed1998.om@gmail.com>
2021-04-16 01:10:50 +0200

    render search results

--- src/views/SearchView.js
+++ src/views/SearchView.js
@@ -1,8 +1,25 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import BooksAPI from '../api'
+import {Book} from '../components'
 
 class SearchView extends Component {
+  state = {
+    searchResults: []
+  }
+
+  handleSearch = (e) => {
+    const searchTerm = e.target.value.trim()
+    BooksAPI.search(searchTerm)
+    .then(r => {
+      this.setState({
+        searchResults: r && r.length > 0? r: []
+      })
+    })
+  } 
   render() {
+    const {searchResults} = this.state
+    const {onShelfChange} = this.props
     return (
       <div className="search-books">
         <div className="search-books-bar">
@@ -11,11 +28,15 @@ class SearchView extends Component {
           </Link>
 
           <div className="search-books-input-wrapper">
-            <input type="text" placeholder="Search by title or author" />
+            <input type="text" placeholder="Search by title or author" onChange={this.handleSearch}/>
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid" />
+          <ol className="books-grid" >
+            {searchResults.map(result => (
+              <li key={result.id}><Book book={result} onShelfChange={onShelfChange}/></li>
+            ))}
+          </ol>
         </div>
       </div>
     );
